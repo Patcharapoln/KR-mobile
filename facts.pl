da84b71887cebffbc70a87b0ba3090b78e9c0472
Jirayu Laungwilawan <jirayu.l@ku.th>
2018-10-06 17:16:33 +0700

    Add fact cameraback

--- facts.pl
+++ facts.pl
@@ -17,3 +17,12 @@ price(asusROG, 39000).
 price(huaweiHonorPlay, 9990).
 price(xiaomiPocoPhoneF1, 10900).
 price(htcU12Life, 13341).
+
+backcamera(blackberryKey, 12).
+backcamera(samsungS9, 12.1).
+backcamera(nokia51, 16.3).
+backcamera(iPhoneXsMax, 12.2).
+backcamera(asusROG, 12.2).
+backcamera(huaweiHonorPlay, 15.9).
+backcamera(xiaomiPocoPhoneF1, 12).
+backcamera(htcU12Life, 16.3).
