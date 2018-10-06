968f751fbcea6b58260d9114db01703b94eeee85
winChawakorn <chawakorn.su@ku.th>
2018-10-06 17:06:46 +0700

    Split fact and rule

--- facts.pl
+++ facts.pl
@@ -1,8 +1,4 @@
-wantedSize(X, S):- size(X, Y), Y < S.
-budget(X, B):- price(X, P), P < B.
-small(X):-size(X,Y), Y<5.
-medium(X):-size(X,Y), Y<6, Y>=5.
-large(X):-size(X,Y), Y>=6.
+:- module(facts,[]).
 
 size(blackberryKey, 4.5).
 size(samsungS9, 5.8).
--- mobile.pl
+++ mobile.pl
@@ -1,11 +1,7 @@
-go :- hypothesize(Phone),
-      write('You should buy '),
-      write(Phone),
-      nl,
-      undo.
+:- use_module(facts,[]).
 
-% hypothesize()
-
-asusPOG :- asus,
-           game,
-           large,
+wantedSize(X, S):- size(X, Y), Y < S.
+budget(X, B):- price(X, P), P < B.
+small(X):-size(X,Y), Y<5.
+medium(X):-size(X,Y), Y<6, Y>=5.
+large(X):-size(X,Y), Y>=6.
